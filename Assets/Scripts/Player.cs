386028b8f671c90db5896bf77352e2e30f206045
Haningyu <haranharan1105@gmail.com>
2025-12-06 23:34:45 +0900

    타일맵 적용

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerMove2D : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpPower = 7f;
+    [SerializeField] LayerMask groundMask;
+    [SerializeField] Transform groundCheck;
+
+    Rigidbody2D rb;
+    float input;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        input = Input.GetAxisRaw("Horizontal");
+        Move();
+    }
+
+    void Move()
+    {
+        bool isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundMask);
+
+        rb.linearVelocity = new Vector2(input * moveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGround)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
     }
 }
