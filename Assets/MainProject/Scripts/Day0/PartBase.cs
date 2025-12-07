573339f1426f2badee1c5c0746c666f28879d0aa
Haran1426 <haranharan1105@gmail.com>
2025-12-08 02:55:43 +0900

    ㄹㅇ 라스트 3차 Day0 작업

--- Assets/MainProject/Scripts/Day0/PartBase.cs
+++ Assets/MainProject/Scripts/Day0/PartBase.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class PartBase : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
-    public Sprite normalSprite;
-    public Sprite highlightSprite;
+    // normalObject는 이제 코드로 끄지 않을 거니까 연결만 잘 되어 있으면 돼요 (삭제해도 됨)
+    public GameObject highlightObject; 
 
     public float detectRange = 1f;
+
+    void Start()
+    {
+        // 시작할 때 하이라이트는 꺼두기
+        if (highlightObject != null) highlightObject.SetActive(false);
+    }
 
     void Update()
     {
@@ -15,12 +20,14 @@ public class PartBase : MonoBehaviour
 
         bool isNear = dist <= detectRange;
 
-        spriteRenderer.sprite = isNear ? highlightSprite : normalSprite;
+        // 가까이 가면 하이라이트만 켰다 껐다 함 (노멀은 안 건드림!)
+        if (highlightObject != null) highlightObject.SetActive(isNear);
 
+        // F키 누르면 '부모 오브젝트'가 꺼지니까, 자식인 노멀도 그때 같이 사라짐
         if (isNear && Input.GetKeyDown(KeyCode.F))
         {
             PartManager.Instance.AddPart();
-            gameObject.SetActive(false);
+            gameObject.SetActive(false); 
         }
     }
 }
--- Assets/MainProject/Scripts/Day0/TimeMachine.cs
+++ Assets/MainProject/Scripts/Day0/TimeMachine.cs
@@ -1,10 +1,13 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TimeMachine : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Sprite normalSprite;
     public Sprite completeSprite;
+
+    // ✨ 추가된 변수: 하이라이트(히팅) 효과 오브젝트
+    public GameObject highlightObject; 
 
     public float detectRange = 4f;
     public GameObject hintText; 
@@ -14,6 +17,9 @@ public class TimeMachine : MonoBehaviour
     void Start()
     {
         hintText.SetActive(false);
+        
+        // 시작할 때 하이라이트는 꺼두기
+        if (highlightObject != null) highlightObject.SetActive(false);
     }
 
     void Update()
@@ -22,12 +28,25 @@ public class TimeMachine : MonoBehaviour
         float dist = Vector2.Distance(transform.position, player.transform.position);
         bool isNear = dist <= detectRange;
 
+        // 1. 상태에 따른 이미지 변경
         if (!isActivated)
             spriteRenderer.sprite = normalSprite;
         else
             spriteRenderer.sprite = completeSprite;
 
-        if (PartManager.Instance.partsCount == 3)
+        // ✨ 2. 거리 감지해서 하이라이트 켜고 끄기 (히팅 효과)
+        // 수리가 안 끝났고(isActivated == false), 가까이 있으면(isNear) 켜짐
+        if (highlightObject != null)
+        {
+            // 수리가 이미 끝났으면 하이라이트 안 나오게 하려면 아래처럼 작성
+            if (isActivated) 
+                highlightObject.SetActive(false);
+            else 
+                highlightObject.SetActive(isNear);
+        }
+
+        // 3. 상호작용 로직 (부품 3개 모았을 때만)
+        if (PartManager.Instance.partsCount == 3 && !isActivated)
         {
             hintText.SetActive(isNear);
 
@@ -35,6 +54,8 @@ public class TimeMachine : MonoBehaviour
             {
                 isActivated = true;
                 hintText.SetActive(false);
+                // 수리 완료 즉시 하이라이트도 끄기
+                if (highlightObject != null) highlightObject.SetActive(false);
             }
         }
         else
