ea580afaa765b073c8bc6b8d739d38625b0c4d31
Hyunwoo Lee <lhs739300@gmail.com>
2025-12-08 03:30:59 +0900

    ADD | 캐릭터 애니메이션 추가

--- Assets/MainProject/Scripts/Player/Player.cs
+++ Assets/MainProject/Scripts/Player/Player.cs
@@ -7,24 +7,38 @@ public class PlayerMove2D : MonoBehaviour
     [SerializeField] LayerMask groundMask;
     [SerializeField] Transform groundCheck;
 
+    private SpriteRenderer spriteRenderer;
+    private Animator anim;
+
     Rigidbody2D rb;
     float input;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         input = Input.GetAxisRaw("Horizontal");
-        if(DialogueSystem.Instance.isDialogueActive) input = 0;
+        if (DialogueSystem.Instance.isDialogueActive) input = 0;
         Move();
     }
 
     void Move()
     {
+        if(input < 0) spriteRenderer.flipX = false;
+        else if(input > 0) spriteRenderer.flipX = true;
+
+        if(input == 0) anim.SetBool("IsMove", false);
+        else anim.SetBool("IsMove", true);
+
         bool isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundMask);
+
+        if(isGround) anim.SetBool("IsOnAir", false);
+        else anim.SetBool("IsOnAir", true);
 
         rb.linearVelocity = new Vector2(input * moveSpeed, rb.linearVelocity.y);
 
