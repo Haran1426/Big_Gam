cac7edfcd1b4ee875b69a699954be6cc5afdaa7e
Hyunwoo Lee <lhs739300@gmail.com>
2025-12-29 13:50:42 +0900

    ADD | 씬에 게임 매니저 추가

--- Assets/MainProject/Scripts/GameManager.cs
+++ Assets/MainProject/Scripts/GameManager.cs
@@ -11,6 +11,19 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     public Language language;
 
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public Language GetLanguageSetting()
     {
         return language;
